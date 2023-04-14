91a90b16e6fcd5c382f1a627eb59483e74f2345c
engthiagos <eng.thiagos@gmail.com>
2023-04-14 10:54:06 -0300

    V1a: create .env / import .env in app.js / import .env in connectDB.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
+// import ENVIRONMENT VARIABLES
+require('dotenv').config()
+const host = process.env.HOST
+const port = process.env.PORT
 // Server Create 
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());                        // express vou trafegar com JSON
-app.listen(3000);
-console.log('**** RODANDO app.js ****');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())                        // express vou trafegar com JSON
+app.listen(port)
+console.log(`**** RODANDO app.js em ${host}:${port} ****`)
 /* SETUP
 set up nodemoon -> packag.json -> scpripts -> "start": "nodemon app.js"
 to start nodemon -> npm start
@@ -15,8 +19,8 @@ Database:   https://www.mongodb.com/atlas/database
             IP: 152.249.142.46/32
 */
 // DB Connection
-const connectDB = require('./db/connectDB');
-connectDB();
+const connectDB = require('./db/connectDB')
+connectDB()
 // Routes
-const routes = require('./routes/router');
-app.use('/api', routes);
+const routes = require('./routes/router')
+app.use('/api', routes)
--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+// import ENVIRONMENT VARIABLES
+require('dotenv').config()
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbID = process.env.DB_ID
+const mongoose = require('mongoose')
 async function main() {
     try {
         await mongoose.connect(
-            "mongodb+srv://PARTYTIME:JreBTzlLvvQDDhE5@cluster0.spcsoty.mongodb.net/?retryWrites=true&w=majority"
-            );
-    console.log('**** Banco de Dados "PARTYTIME" conectado ****');
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.spcsoty.mongodb.net/?retryWrites=true&w=majority`)
+    console.log('**** Banco de Dados "PARTYTIME" conectado ****')
     } catch (error) {
-        console.log(`Erro: ${error.message}`);
+        console.log(`Erro: ${error.message}`)
     }
 }
-module.exports = main;
+module.exports = main

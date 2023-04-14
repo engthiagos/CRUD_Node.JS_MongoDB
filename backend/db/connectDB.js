a5d01d5d9a82c8a8408e841e6a5c2d7df5115a99
engthiagos <eng.thiagos@gmail.com>
2023-04-14 17:42:08 -0300

    V01.02a - create DB  connection with MongoDB Drive

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,14 +1,43 @@
 // import ENVIRONMENT VARIABLES
 require('dotenv').config()
+const dbProtocol = process.env.DB_PROTOCOL
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
+const dbHost = process.env.DB_HOST
+const dbConnectionOptions = process.env.DB_CONNECTION_OPTIONS
 const dbID = process.env.DB_ID
+// import MongoDB Drive
+const { MongoClient, ServerApiVersion } = require("mongodb")
+const uri = `${dbProtocol}://${dbUser}:${dbPassword}@${dbHost}/${dbConnectionOptions}`
+// create MongoClient with more stable version
+const client = new MongoClient(uri,  {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+// connect wiht MongoDB Drive
+async function run() {
+    try {
+      // Connect the client to the server (optional starting in v4.7)
+    //   await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log("**** Banco de Dados ${dbUser} conectado pelo MongoDB Driver ****");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+// MONGOOSE
 const mongoose = require('mongoose')
 async function main() {
     try {
-        await mongoose.connect(
-            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.spcsoty.mongodb.net/?retryWrites=true&w=majority`)
-    console.log('**** Banco de Dados "PARTYTIME" conectado ****')
+        await mongoose.connect(`${dbProtocol}://${dbUser}:${dbPassword}@${dbHost}/${dbConnectionOptions}`)
+    console.log(`**** Banco de Dados ${dbUser} conectado pelo MONGOOSE ****`)
     } catch (error) {
         console.log(`Erro: ${error.message}`)
     }
